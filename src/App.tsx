a6f7cc0ac744a3585b53753f13abff68c3d76fa4
Ayush Maurya <ayushmaurya2003@gmail.com>
2026-03-30 01:00:26 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="container mx-auto p-8 text-center relative z-10">
+      <div className="flex justify-center items-center gap-8 mb-8">
+      </div>
+        <h1 className="text-4xl font-bold mb-4">Representing India</h1>
+        <p className="text-lg text-gray-600 mb-8">
+          Coming Soon...
+        </p>
+        <br />
+        <p className="text-sm text-gray-500">
+          This research is currently under development.
+          
+        <br /> 
+          Authors: Ayush Maurya, Dr. Aniket Alam
+        </p>
+    </div>
+  );
+}
+
+export default App;
